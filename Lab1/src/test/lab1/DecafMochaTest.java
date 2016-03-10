bca911009eb64bf9e646b5dac9ccca40302491eb
billy191 <14302010007@fudan.edu.cn>
2016-03-10 18:46:57 +0800

    3.1_Decaf_Mocha_test_statistic_changes

--- Lab1/src/test/lab1/DecafMochaTest.java
+++ Lab1/src/test/lab1/DecafMochaTest.java
@@ -18,13 +18,13 @@ public class DecafMochaTest {
     @Test
     public void testSizeCost(){
         decafMocha.setSize("small");
-        Assert.assertEquals(1.4,decafMocha.cost(),0);
+        Assert.assertEquals(1.2,decafMocha.cost(),0);
         decafMocha.setSize("medium");
-        Assert.assertEquals(1.7,decafMocha.cost(),0);
+        Assert.assertEquals(1.5,decafMocha.cost(),0);
         decafMocha.setSize("large");
-        Assert.assertEquals(2,decafMocha.cost(),0);
+        Assert.assertEquals(1.8,decafMocha.cost(),0);
         decafMocha.setSize("grant");
-        Assert.assertEquals(2.3,decafMocha.cost(),0);
+        Assert.assertEquals(2.1,decafMocha.cost(),0);
     }
 
     @Test
